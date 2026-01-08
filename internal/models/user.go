e2cf7bc35e0413bb7402c6bfd4673bbb491694bc
ahmed27-droid <arhimedme@gmail.com>
2026-01-08 12:36:50 +0300

    создал структкру user

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,19 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name  string `json:"name" gorm:"not null;size:20;uniqueIndex"`
+	Email string `json:"email" gorm:"uniqueIndex;not null;size:50"`
+}
+
+type CreateUser struct {
+	Name string `json:"name" binding:"required"`
+	Email string `json:"email" binding:"required"`
+}
+
+type UpdateUser struct{
+	Name string `json:"name" binding:"max=20"`
+	Email string `json:"email" binding:"max=50"`
+}
