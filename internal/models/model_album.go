88c66734c57f2eeb750f70aeeab600af16dc9b7c
Mountainman199231 <arizonaarizona8477@gmail.com>
2026-01-08 15:03:13 +0300

    Features/model album (#4)
    * Создал структуру и дто Artist
    
    * сделал структуру и дто Альбома
    
    ---------
    
    Co-authored-by: mountainman199231 <arizonaarizona@mail.ru>

---
+++ internal/models/model_album.go
@@ -0,0 +1,29 @@
+package models
+
+import "gorm.io/gorm"
+
+type Album struct {
+	gorm.Model
+
+	Title string `json:"title" gorm:"not null;size:20;uniqueIndex:idx_artist_title"`
+	Year  int    `json:"year" gorm:"not null"`
+
+	ArtistID uint   `json:"artist_id" gorm:"not null;uniqueIndex:idx_artist_title"`
+	Artist   Artist `json:"-" gorm:"constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+
+	Tracks []Track `json:"tracks,omitempty" gorm:"foreignKey:AlbumID;constraint:OnDelete:CASCADE"`
+}
+
+type AlbumRequest struct {
+	Title string `json:"title" binding:"required,min=2,max=20"`
+	Year  int    `json:"year" binding:"required,gte=1950,lte=2025"`
+
+	ArtistID uint `json:"artist_id" binding:"required"`
+}
+
+
+type AlbumUpdateRequest struct{
+	ArtistID *uint `json:"artist_id" binding:"omitempty"`
+	Title *string `json:"title" binding:"omitempty,min=2,max=20"`
+	Year  *int	 `json:"year" binding:"omitempty,gte=1950,lte=2025"`
+}
