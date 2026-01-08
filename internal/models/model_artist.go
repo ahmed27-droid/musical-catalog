f7a6060c014ed210b9cb2777738f5ea22cb346b4
Mountainman199231 <arizonaarizona8477@gmail.com>
2026-01-08 21:43:28 +0300

    Features/artist service (#9)
    * Создал структуру и дто Artist
    
    * сделал структуру и дто Альбома
    
    * artist _repository
    
    * сделал репо на Албом
    
    * сделал артист сервис
    
    ---------
    
    Co-authored-by: mountainman199231 <arizonaarizona@mail.ru>

--- internal/models/model_artist.go
+++ internal/models/model_artist.go
@@ -5,13 +5,17 @@ import "gorm.io/gorm"
 type Artist struct {
 	gorm.Model
 
-	Name string `json:"name" gorm:"not null;size:20;uniqueIndex"`
-	Bio	 string	`json:"bio" gorm:"not null;size:100"`
+	Name   string  `json:"name" gorm:"not null;size:20;uniqueIndex"`
+	Bio    string  `json:"bio" gorm:"not null;size:100"`
 	Albums []Album `json:"albums,omitempty" gorm:"foreignKey:ArtistID"`
-
 }
 
-type ArtistRequest struct{
+type ArtistCreateRequest struct {
 	Name string `json:"name" binding:"required,min=2,max=20"`
 	Bio  string `json:"bio" binding:"required,min=10,max=100"`
+}
+
+type ArtistUpdateRequest struct {
+	Name *string `json:"name" binding:"omitempty,min=2,max=20"`
+	Bio  *string `json:"bio" binding:"omitempty,min=10,max=100"`
 }
--- internal/services/artist_service.go
+++ internal/services/artist_service.go
@@ -1,1 +1,76 @@
 package services
+
+import (
+	"musical-catalog/internal/models"
+	"musical-catalog/internal/repository"
+)
+
+type ArtistService interface {
+	CreateArtist(req models.ArtistCreateRequest) (*models.Artist, error)
+	GetArtist(id uint) (*models.Artist, error)
+	UpdateArtist(id uint, req models.ArtistUpdateRequest) (*models.Artist, error)
+	DeleteArtist(id uint) error
+	ListArtist() ([]models.Artist, error)
+}
+
+type artistService struct{
+	artistRepo repository.ArtistRepository
+}
+
+func NewArtistService(
+	artistRepo repository.ArtistRepository, 
+	) ArtistService{
+		return &artistService{
+			artistRepo: artistRepo, 
+		}
+	}
+
+func (s *artistService) CreateArtist(req models.ArtistCreateRequest) (*models.Artist, error){
+	
+	artist := models.Artist{
+		Name: req.Name,
+		Bio: req.Bio,
+	}
+
+	if err := s.artistRepo.Create(&artist); err != nil{
+		return nil, err
+	}
+
+	return &artist, nil
+}
+
+func (s *artistService) GetArtist(id uint) (*models.Artist, error) {
+	return s.artistRepo.GetByID(id)
+}
+
+func (s *artistService) UpdateArtist(id uint, req models.ArtistUpdateRequest) (*models.Artist, error) {
+	artist, err := s.artistRepo.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.Name != nil{
+		artist.Name = *req.Name		
+	}
+
+	if req.Bio != nil {
+		artist.Bio = *req.Bio
+	}
+
+	if err := s.artistRepo.Update(artist); err != nil{
+		return nil, err
+	}
+	return artist, nil
+}
+
+func (s *artistService) DeleteArtist(id uint) error{
+	if _, err := s.artistRepo.GetByID(id); err != nil {
+		return err
+	}
+
+	return s.artistRepo.Delete(id)
+}
+
+func (s *artistService) ListArtist() ([]models.Artist, error) {
+	return s.artistRepo.List()
+}
