b722554f683d06e1928f32a3ae6b6d5d3dc107f8
ahmed27-droid <arhimedme@gmail.com>
2026-01-09 15:39:17 +0300

    сделал transport для user (#17)

--- internal/transport/user_transport.go
+++ internal/transport/user_transport.go
@@ -1,1 +1,94 @@
 package transport
+
+import (
+	"musical-catalog/internal/models"
+	"musical-catalog/internal/services"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserTransport struct {
+	service services.UserService
+}
+
+func NewUserService(service services.UserService) UserTransport {
+	return &UserTransport{service: service}
+}
+
+func (t *UserTransport) Create(c *gin.Context) {
+	var req models.CreateUser
+
+	if err := c.ShouldBindJSON(req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := t.service.CreateUser(req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, user)
+}
+
+func (t *UserTransport) GetByID(c *gin.Context) {
+	id, err :=strconv.Atoi(c.Param("id"))
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":"invalid id"})
+		return
+	}
+
+	user, err := t.service.GetByIDUser(uint(id))
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (t *UserTransport) Update(c *gin.Context) {
+	id, err :=strconv.Atoi(c.Param("id"))
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":"invalid id"})
+		return
+	}
+	var req models.UpdateUser
+	if err := c.ShouldBindJSON(&req); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		return
+	}
+
+	user, err := t.service.UpdateUser(uint(id), req)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		return 
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func (t *UserTransport) Delete(c *gin.Context) {
+	id, err :=strconv.Atoi(c.Param("id"))
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":"invalid id"})
+		return
+	}
+
+	if err := t.service.DeleteUser(uint(id)); err !=nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message":"user delete"})
+}
+
+func(t *UserTransport) List(c *gin.Context) {
+	users, err := t.service.ListUser()
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
+}
