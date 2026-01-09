7f773440648fe58603e85a8232a8ba944c35a6e1
Mountainman199231 <arizonaarizona8477@gmail.com>
2026-01-09 15:39:36 +0300

    Features/routes (#16)
    * Создал структуру и дто Artist
    
    * сделал структуру и дто Альбома
    
    * artist _repository
    
    * сделал репо на Албом
    
    * сделал артист сервис
    
    * сделал сервисы для артист и альбом
    
    * сделал artist transport
    
    * создал album transport
    
    * создал руты
    
    ---------
    
    Co-authored-by: mountainman199231 <arizonaarizona@mail.ru>

--- internal/transport/routes.go
+++ internal/transport/routes.go
@@ -1,1 +1,26 @@
 package transport
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(
+	router *gin.Engine,
+	artistTransport *ArtistTransport,
+	albumTransport *AlbumTransport,
+	
+){
+	router.GET("/artists",artistTransport.ListArtists)
+	router.POST("/artists", artistTransport.Create)
+
+	router.GET("/artists/:id", artistTransport.GetArtistByID)
+	router.PATCH("/artists/:id", artistTransport.UpdateArtist)
+	router.DELETE("/artists/:id", artistTransport.DeleteArtist)
+
+	router.GET("/albums", albumTransport.ListAlbums)
+	router.POST("/albums", albumTransport.CreateAlbum)
+
+	router.GET("/albums/:id", albumTransport.GetAlbumByID)
+	router.PATCH("/albums/:id", albumTransport.UpdateAlbum)
+	router.DELETE("/albums/:id", albumTransport.DeleteAlbum)
+	router.GET("/albums/:id/average", albumTransport.GetAlbumsAverageRating)
+
+}
