0e76ef8a319770d7595ae0ec1d19c891619ae754
mountainman199231 <arizonaarizona@mail.ru>
2026-01-09 15:34:27 +0300

    добавил в майн

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "musical-catalog/internal/config"
+import (
+	"musical-catalog/internal/config"
+	"musical-catalog/internal/models"
+	"musical-catalog/internal/repository"
+	"musical-catalog/internal/services"
+	"musical-catalog/internal/transport"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	db := config.SetupDatabase()
 
-	db.AutoMigrate()
+	db.AutoMigrate(&models.Artist{}, &models.Album{})
+
+	artistRepo := repository.NewArtistRepository(db)
+	albumRepo := repository.NewAlbumRepository(db)
+
+	artistService := services.NewArtistService(artistRepo, albumRepo)
+	albumService := services.NewAlbumService(artistRepo, albumRepo)
+
+	artistTransport := transport.NewArtistTransport(artistService)
+	albumTransport := transport.NewAlbumTransport(albumService)
+
+	router := gin.Default()
+	transport.RegisterRoutes(router, artistTransport, albumTransport)
+	router.Run(":8080")
 
 }
