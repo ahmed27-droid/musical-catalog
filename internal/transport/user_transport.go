f518eafd2f6ad73a1e0a946720f11995fbc46050
mountainman199231 <arizonaarizona@mail.ru>
2026-01-12 11:34:21 +0300

    ddd

--- internal/transport/user_transport.go
+++ internal/transport/user_transport.go
@@ -20,7 +20,7 @@ func NewUserTransport(service services.UserService) *UserTransport {
 func (t *UserTransport) Create(c *gin.Context) {
 	var req models.CreateUser
 
-	if err := c.ShouldBindJSON(req); err != nil {
+	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -34,59 +34,59 @@ func (t *UserTransport) Create(c *gin.Context) {
 }
 
 func (t *UserTransport) GetByID(c *gin.Context) {
-	id, err :=strconv.Atoi(c.Param("id"))
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":"invalid id"})
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
 		return
 	}
 
 	user, err := t.service.GetByIDUser(uint(id))
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, user)
 }
 
 func (t *UserTransport) Update(c *gin.Context) {
-	id, err :=strconv.Atoi(c.Param("id"))
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":"invalid id"})
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
 		return
 	}
 	var req models.UpdateUser
-	if err := c.ShouldBindJSON(&req); err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	user, err := t.service.UpdateUser(uint(id), req)
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
-		return 
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 
 	c.JSON(http.StatusOK, user)
 }
 
 func (t *UserTransport) Delete(c *gin.Context) {
-	id, err :=strconv.Atoi(c.Param("id"))
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":"invalid id"})
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
 		return
 	}
 
-	if err := t.service.DeleteUser(uint(id)); err !=nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+	if err := t.service.DeleteUser(uint(id)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message":"user delete"})
+	c.JSON(http.StatusOK, gin.H{"message": "user delete"})
 }
 
-func(t *UserTransport) List(c *gin.Context) {
+func (t *UserTransport) List(c *gin.Context) {
 	users, err := t.service.ListUser()
-	if err != nil{
-		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
