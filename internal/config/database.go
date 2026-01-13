9ae3ddceec9cf0390b13fe879be072e2e40e86dc
ahmed27-droid <arhimedme@gmail.com>
2026-01-13 10:07:20 +0300

    изменил sataus

--- internal/config/database.go
+++ internal/config/database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func SetupDatabase() *gorm.DB {
-	dsn := "host=localhost user=postgres password=7213 dbname=musical-catalog port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=arhimed2734 dbname=musical-catalog port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("не удалось подключиться к БД: " + err.Error())
